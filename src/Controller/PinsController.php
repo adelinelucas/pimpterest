b631aa560fb2525cff0d9e58e5d063324dc0df30
adelinelucas <82570582+adelinelucas@users.noreply.github.com>
2023-07-12 15:00:32 +0200

    Setup PinsController, Add shch bundle, pluralize twig function

---
+++ src/Controller/PinsController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\PinRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PinsController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_home")
+     */
+    public function index(PinRepository $pinRepo): Response
+    {
+        $pins = $pinRepo->findAll();
+
+        return $this->render('pins/index.html.twig',[
+            'pins' => $pins
+        ]);
+    }
+}
