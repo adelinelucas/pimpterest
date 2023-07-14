b4798f12ea40019a60007b13b62e26b0a72a66f7
adelinelucas <82570582+adelinelucas@users.noreply.github.com>
2023-07-14 19:08:03 +0200

    add user entity, User repository and generate migration

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
