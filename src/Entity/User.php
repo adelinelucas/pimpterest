bef73c60e84e383ac1e94fd0536d9134718de118
adelinelucas <82570582+adelinelucas@users.noreply.github.com>
2023-07-15 17:35:21 +0200

    display pin owner and add getFullName for user entity for full name generation

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -205,4 +205,8 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         return $this;
     }
 
+    public function getFullName(): string 
+    {
+        return $this->getFirstName() .' '. $this->getLastName();
+    }
 }
