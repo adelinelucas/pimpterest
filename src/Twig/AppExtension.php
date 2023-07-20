5551807a80738c8370ea1a8fb91cb5eff1be508c
adelinelucas <82570582+adelinelucas@users.noreply.github.com>
2023-07-20 23:19:46 +0200

    test use of security dependencie in appExtension

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -2,6 +2,7 @@
 
 namespace App\Twig;
 
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\String\Inflector\EnglishInflector;
 use Symfony\Component\String\Slugger\SluggerInterface;
 use Twig\Extension\AbstractExtension;
@@ -10,6 +11,13 @@ use Twig\TwigFunction;
 
 class AppExtension extends AbstractExtension
 {
+    // comme on est pas dans un controller on a pas directement accès au user, par contre on peut injecter des dépendances pour avoir acc_s aux infos que l'on souhaite
+    private $security;
+    public function __construct(Security $security)
+    {
+        $user = $security->getUser();
+        // dd($security->isGranted('ROLE_ADMIN'));
+    }
     public function getFilters(): array
     {
         return [
